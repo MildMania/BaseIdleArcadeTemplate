19c5f2e7852f6457eace8262e2c1e97717320e02
Okan Tekdas <okan.tekdas@mildmania.com>
2022-07-01 13:29:54 +0300

    post build method updated

--- Assets/Editor/CloudBuildHelper.cs
+++ Assets/Editor/CloudBuildHelper.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEditor;
 using System;
+using System.IO;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
 
 public class CloudBuildHelper : MonoBehaviour
 {
@@ -13,4 +16,45 @@ public class CloudBuildHelper : MonoBehaviour
         PlayerSettings.iOS.buildNumber = buildNumber;
     }
 #endif
+
+    [PostProcessBuild(1)]
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+    {
+        if (buildTarget == BuildTarget.iOS)
+        {
+            // ModifyFrameworks(path);
+            ModifyPlist(path);
+        }
+    }
+
+    private static void ModifyPlist(string path)
+    {
+        string plistPath = path + "/Info.plist";
+        Debug.Log("Plist path: " + plistPath);
+       
+        PlistDocument plistDocument = new PlistDocument();
+        plistDocument.ReadFromString(File.ReadAllText(plistPath));
+        PlistElementDict plistDocumentRootDict = plistDocument.root;
+        plistDocumentRootDict.SetBoolean("ITSAppUsesNonExemptEncryption", false);
+
+    }
+
+    private static void ModifyFrameworks(string path)
+    {
+        string projPath = PBXProject.GetPBXProjectPath(path);
+
+        var project = new PBXProject();
+        project.ReadFromFile(projPath);
+
+        string mainTargetGuid = project.GetUnityMainTargetGuid();
+
+        foreach (var targetGuid in new[] {mainTargetGuid, project.GetUnityFrameworkTargetGuid()})
+        {
+            project.SetBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "NO");
+        }
+
+        project.SetBuildProperty(mainTargetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+
+        project.WriteToFile(projPath);
+    }
 }
